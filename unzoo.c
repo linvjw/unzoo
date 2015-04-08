8be419e27f137fbd0580a9631e1d8c836ef277f9
John W. Linville <linville@tuxdriver.com>
2015-04-08 13:53:31 -0400

    Fix _most_ of the compiler warnings
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- unzoo.c
+++ unzoo.c
@@ -247,7 +247,12 @@
 *H
 */
 #include        <stdio.h>
+#include        <stdlib.h>
 #include        <string.h>
+
+#include        <sys/stat.h>
+#include        <sys/types.h>
+#include        <utime.h>
 
 
 /****************************************************************************
@@ -617,8 +622,8 @@ char            Cmd [256];
 **  members with the correct time as stored in the archive.
 */
 #ifdef  SYS_IS_UNIX
-unsigned int   Secs [2];
-#define SETF_TIME(patl,secs)    (Secs[0]=Secs[1]=(secs),!utime((patl),Secs))
+struct  utimbuf Secs;
+#define SETF_TIME(patl,secs)    (Secs.actime=Secs.modtime=(secs),!utime((patl),&Secs))
 #endif
 #ifdef  SYS_IS_DOS_DJGPP
 unsigned long   Secs [2];
@@ -1469,7 +1474,7 @@ struct {
 int             DescReadArch ()
 {
     /* read the text at the beginning                                      */
-    BlckReadArch(Descript.text,20L);  Descript.text[20] = '\0';
+    BlckReadArch(Descript.text,20L);  Descript.text[19] = '\0';
 
     /* try to read the magic words                                         */
     if ( (Descript.magic = WordReadArch()) != (unsigned long)0xfdc4a7dcL )
